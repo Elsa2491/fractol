77795f5cd88ed4ec73072a47424d0c25a81edfa0
Elsa2491 <elsa.touma@gmail.com>
2023-12-23 20:25:22 +0100

    add files

--- maths_functions.c
+++ maths_functions.c
@@ -1,6 +1,6 @@
 #include "fractol.h"
 
-double	ft_to_scale(double unscaled_n, double new_min, double new_max, double old_max)
+double	ft_to_scale(double n, double new_min, double new_max, double old_max)
 {
 	return ((new_max - new_min) * unscaled_n / old_max + new_min);
 }
